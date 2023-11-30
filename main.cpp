62a36324ae90c8f1005d69ff9876d691f3ee282d
MagnoDutra <jmagno.dutra@hotmail.com>
2023-11-29 22:14:30 -0300

    Drawing the character

--- main.cpp
+++ main.cpp
@@ -8,8 +8,15 @@ int main(){
 
     Texture2D worldMap = LoadTexture("./nature_tileset/OpenWorldMap24x24.png");
     Vector2 mapPos{0.0, 0.0};
-
     float speed{4.0};
+
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        (float)windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
+        (float)windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height)
+    };
+    Rectangle spriteSource{0, 0, (float)knight.width/6.0f, (float)knight.height};
+    Rectangle spriteDest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -23,11 +30,15 @@ int main(){
         if(IsKeyDown(KEY_W)) direction.y -= 1.0;
         if(IsKeyDown(KEY_S)) direction.y += 1.0;
 
-        if(Vector2Length(direction) != 0.0){
-            mapPos = Vector2Subtract(mapPos, Vector2Normalize(direction)), speed);
+        if(Vector2Length(direction) != 0.0){            
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
         }
         
+        // Draw Map
         DrawTextureEx(worldMap, mapPos, 0, 4.0, WHITE);
+
+        // Draw Knight
+        DrawTexturePro(knight, spriteSource, spriteDest, Vector2{}, 0.f, WHITE);
         EndDrawing();
     }
     UnloadTexture(worldMap);
